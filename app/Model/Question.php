868d995f1f12c41621caf2e55760e6155eb0250a
samadnaghipourbit <samad.naghipour@yahoo.com>
2020-04-10 08:23:10 +0430

    RelationShips

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -4,7 +4,24 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Reply;
+use App\Model\Category;
+use App\User;
+
 class Question extends Model
 {
     //
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(){
+
+        return $this->hasMany(Reply::class);
+    }
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,25 @@
 
 namespace App\Model;
 
+
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\Model\Question;
+use App\Model\Like;
 class Reply extends Model
 {
-    //
+    
+    public function question(){
+
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
