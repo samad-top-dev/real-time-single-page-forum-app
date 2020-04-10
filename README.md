f4b72ede976bae847a6587a1893c4f5fd64eb399
samadnaghipourbit <samad.naghipour@yahoo.com>
2020-04-10 07:13:06 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is real time single page forum app
+
+## We are going o use VueJs, Pusher, Vuetify, Laravel, JWT and Markdown
---
+++ database/migrations/2020_04_09_160621_create_replies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id');
+            // creating a key to questions table to delete the reply record after a question deleting
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
